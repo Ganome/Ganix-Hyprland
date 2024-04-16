a847323a756edc302241fe680f1c971b164ca8e7
Ross Dole <ganome@gmail.com>
2024-04-16 10:17:51 -0600

    Updated Hyprland Keybinding helper

--- config/scripts/list-hypr-bindings.nix
+++ config/scripts/list-hypr-bindings.nix
@@ -4,7 +4,7 @@ let
   inherit ( import ../../hosts/${host}/options.nix ) terminal browser;
 in
 pkgs.writeShellScriptBin "list-hypr-bindings" ''
-  yad --width=800 --height=650 \
+  yad --width=925 --height=650 \
   --center \
   --fixed \
   --title="Hyprland Keybindings" \
@@ -17,41 +17,25 @@ pkgs.writeShellScriptBin "list-hypr-bindings" ''
   --timeout-indicator=right \
   " = Windows/Super/CAPS LOCK" "Modifier Key, used for keybindings" "Doesn't really execute anything by itself." \
   " + ENTER" "Terminal" "${terminal}" \
-  " + SHIFT + ENTER" "Rofi App Launcher" "rofi -show drun" \
-  " + Q" "Kill Focused Window" "killactive" \
+  " + P" "Rofi App Launcher" "rofi -show drun" \
+  " + SHIFT + Q" "Kill Focused Window" "killactive" \
   " + SHIFT + W" "Search Websites Like Nix Packages" "web-search" \
-  " + SHIFT + N" "Reload SwayNC Styling" "swaync-client -rs" \
   " + W" "Launch Web Browser" "${browser}" \
   " + E" "Launch Emoji Selector" "emopicker9000" \
+  " + B" "Hide waybar temporarily" "pkill -10 waybar" \
+  " + SHIFT + B" "Restart waybar" "reset-waybar" \
   " + S" "Take Screenshot" "screenshootin" \
-  " + D" "Launch Discord" "discord" \
-  " + O" "Launch OBS" "obs" \
-  " + G" "Launch GIMP" "gimp" \
-  " + N" "Launch New File Browser Window" "thunar" \
+  " + D" "Launch webcord" "webcord" \
   " + M" "Launch Spotify" "spotify" \
   " + P" "Pseudo Tiling" "pseudo" \
   " + SHIFT + I" "Toggle Split Direction" "togglesplit" \
   " + F" "Toggle Focused Fullscreen" "fullscreen" \
-  " + SHIFT + F" "Toggle Focused Floating" "fullscreen" \
-  " + SHIFT + C" "Quit / Exit Hyprland" "exit" \
-  " + Left" "Move Focus To Window On The Left" "movefocus,l" \
-  " + Right" "Move Focus To Window On The Right" "movefocus,r" \
-  " + Up" "Move Focus To Window On The Up" "movefocus,u" \
-  " + Down" "Move Focus To Window On The Down" "movefocus,d" \
-  " + SHIFT + Left" "Move Focused Window Left" "movewindow,l" \
-  " + SHIFT + Right" "Move Focused Window Right" "movewindow,r" \
-  " + SHIFT + Up" "Move Focused Window Up" "movewindow,u" \
-  " + SHIFT + Down" "Move Focused Window Down" "movewindow,d" \
-  " + H" "Move Focus To Window On The Left" "movefocus,l" \
-  " + L" "Move Focus To Window On The Right" "movefocus,r" \
-  " + K" "Move Focus To Window On The Up" "movefocus,u" \
-  " + J" "Move Focus To Window On The Down" "movefocus,d" \
-  " + SHIFT + H" "Move Focused Window Left" "movewindow,l" \
-  " + SHIFT + L" "Move Focused Window Right" "movewindow,r" \
-  " + SHIFT + K" "Move Focused Window Up" "movewindow,u" \
-  " + SHIFT + J" "Move Focused Window Down" "movewindow,d" \
-  " + SPACE" "Toggle Special Workspace" "togglespecialworkspace" \
-  " + SHIFT + SPACE" "Send Focused Window To Special Workspace" "movetoworkspace,special" \
+  " + SHIFT + SPACE" "Toggle Focused Floating" "fullscreen" \
+  " + SHIFT + E" "Quit / Exit Hyprland" "exit" \
+  " + CTRL + Left" "Move Focused Window Left Monitor" "hyprctl dispatch 'split-changemonitor prev'" \
+  " + CTRL + Right" "Move Focused Window to Right monitor" "hyprctl dispatch 'split-changemonitor next'" \
+  " + MINUS" "Toggle Special Workspace" "togglespecialworkspace" \
+  " + SHIFT + MINUS" "Send Focused Window To Special Workspace" "movetoworkspace,special" \
   " + 1-0" "Move To Workspace 1 - 10" "workspace,X" \
   " + SHIFT + 1-0" "Move Focused Window To Workspace 1 - 10" "movetoworkspace,X" \
   " + MOUSE_LEFT" "Move/Drag Window" "movewindow" \
